4bf2ed5fe8b78eb22256da198dbb2565b5992f17
Anish Dhawan <ad99@uw.edu>
2018-05-23 20:17:02 -0700

    Done With Sidebar

--- server.R
+++ server.R
@@ -6,13 +6,18 @@ library(ggplot2)
 library(shiny)
 library(leaflet)
 library(plotly)
+library(maps)
 
 # Reads in data
-honey_production_data <- read.csv(file = "honeyproduction.csv", stringsAsFactors = FALSE)
+honey_production_data <- read.csv(file = "data/honeyproduction.csv", stringsAsFactors = FALSE)
+honey_production_data_long <- gather(honey_production_data,
+                                     key = data,
+                                     value = value,
+                                     numcol, yieldpercol, totalprod)
 
 
 my_server <- function(input, output, session) {
-
+  
 }
 
 shinyServer(my_server)
--- ui.R
+++ ui.R
@@ -10,13 +10,21 @@ my_ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Home", tabName = "home", icon = icon("home")),
-      menuItem("Nation", tabName = "nation", icon = icon("map")),
+      menuItem("Nation", tabName = "nation", icon = icon("map"),
+               selectInput("datachoice", label = "Data",
+                           honey_production_data_long$data),
+               selectInput("yearchoice", label = "Year",
+                           honey_production_data_long$year)),
       menuItem("State", tabName = "state", icon = icon("map-marker")),
       menuItem("Summary", tabName = "summary", icon = icon("book")),
       menuItem("Reference", tabName = "refer", icon = icon("graduation-cap"))
     )
   ),
-  dashboardBody(),
+  dashboardBody(
+    fluidRow(
+      leafletOutput("plot")
+    )
+  ),
   skin = "yellow"
 )
 
