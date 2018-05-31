3fe02afd4e7f780d6e7ae0d0ea1b3fe22eeee72f
Keegan Hanes <keeganhanes4@gmail.com>
2018-05-30 19:57:22 -0700

    Add comments and format code

--- server.R
+++ server.R
@@ -14,66 +14,64 @@ library(geojson)
 
 # Reads in data
 honey_production_data <- read.csv(file = "data/honeyproduction.csv", stringsAsFactors = FALSE)
-honey_production_data_long <- gather(honey_production_data,
-                                     key = data,
-                                     value = value,
-                                     numcol, yieldpercol, totalprod)
 
 my_server <- function(input, output, session) {
   map <- reactive({
     correct_state <- state.abb[match(input$obs_state, state.name)]
-    select(filter(honey_production_data, year >= input$obs_year & correct_state == state), year, state, 
-           totalprod, numcol)
+    select(
+      filter(honey_production_data, year >= input$obs_year & correct_state == state), year, state,
+      totalprod, numcol
+    )
   })
-  
+
   graph <- reactive({
-    column_names <- c("State", "# of Colonies", "Yield", "Production", "Stocks", "Price (per LB)", 
-                      "Prod Value", "Year")
+    column_names <- c(
+      "State", "# of Colonies", "Yield", "Production", "Stocks", "Price (per LB)",
+      "Prod Value", "Year"
+    )
     names(honey_production_data) <- column_names
     correct_state <- state.abb[match(input$obs_state, state.name)]
     filter(honey_production_data, Year >= input$obs_year & correct_state == State)
   })
-  
+
   country_average <- reactive({
-    honey_production_data %>% 
-      filter(year >= input$obs_year) %>% 
-      group_by(year) %>% 
+    honey_production_data %>%
+      filter(year >= input$obs_year) %>%
+      group_by(year) %>%
       summarize(col = mean(numcol))
   })
-  
+
   bins <- c(100000, 1000000, 5000000, 10000000, 20000000, 30000000, 40000000, 50000000)
-  
+
   map_viz <- reactive({
-    
     # Filters the data so that it could be mapped correctly.
     states <- geojson_read("data/us_states.json", what = "sp")
     names(states@data) <- "state"
-    single_year <- honey_production_data %>% 
+    single_year <- honey_production_data %>%
       filter(year == input$obs_year)
     single_year$state <- state.name[match(single_year$state, state.abb)]
-    states@data <- left_join(states@data, single_year, by = "state") 
-    
+    states@data <- left_join(states@data, single_year, by = "state")
+
     # Orders the bins and colors that will be used for the map, and creates labels as well.
     pal <- colorBin("YlOrRd", domain = states@data$totalprod, bins = bins)
     labels <- sprintf(
       "<strong>%s</strong><br/>%g Lbs produced",
       states@data$state, states@data$totalprod
     ) %>% lapply(htmltools::HTML)
-    
+
     # Gives the center of a given state
     coords <- data.frame(state.name, state.center)
     center <- filter(coords, state.name == input$obs_state)
-    
+
     # Creates the map visualization
     leaflet(states) %>%
       setView(center$x, center$y, 5) %>%
       addProviderTiles("MapBox", options = providerTileOptions(
         id = "mapbox.light",
-        accessToken = Sys.getenv('pk.eyJ1IjoiaGFyam90ZCIsImEiOiJjamhxajlxMHkwMnhyMzZwcHg0OXB4b
-                                 TlmIn0.xCU7EJo0WQ1jW-CvwFhfGw'))
-        ) %>%  
+        accessToken = Sys.getenv("pk.eyJ1IjoiaGFyam90ZCIsImEiOiJjamhxajlxMHkwMnhyMzZwcHg0OXB4b\n                                 TlmIn0.xCU7EJo0WQ1jW-CvwFhfGw")
+      )) %>%
       addPolygons(
-        fillColor = ~pal(states@data$totalprod),
+        fillColor = ~ pal(states@data$totalprod),
         color = "white",
         dashArray = "3",
         fillOpacity = "0.7",
@@ -81,35 +79,38 @@ my_server <- function(input, output, session) {
           weight = 5,
           color = "#666",
           dashArray = "",
-          fillOpacity = 0.7, 
+          fillOpacity = 0.7,
           bringToFront = TRUE
-        ), 
+        ),
         label = labels,
         labelOptions = labelOptions(
           style = list("font-weight" = "normal", padding = "3px 8px"),
           textsize = "15px",
-          direction = "auto")) %>%
+          direction = "auto"
+        )
+      ) %>%
       addLegend(
-        pal = pal, values = ~states@data$totalprod, opacity = 0.7, title = NULL,
+        pal = pal, values = ~ states@data$totalprod, opacity = 0.7, title = NULL,
         position = "bottomright"
       )
-    
   })
-  
+
   output$country <- renderLeaflet(
     map_viz()
   )
-  
+
   output$graph_output <- renderPlot({
-    ggplot(map(), mapping = aes(year, numcol, color = "red")) + 
+    ggplot(map(), mapping = aes(year, numcol, color = "red")) +
       geom_point() +
-      geom_line() + 
+      geom_line() +
       geom_smooth(country_average(), mapping = aes(year, col), color = "black") +
-      labs(x = "Year", y = "# of Colonies", 
-           title = "Colonies") + 
+      labs(
+        x = "Year", y = "# of Colonies",
+        title = "Colonies"
+      ) +
       theme(legend.position = "none")
   })
-  
+
   output$table <- renderTable({
     graph()
   })
@@ -121,12 +122,12 @@ my_server <- function(input, output, session) {
       scale_x_continuous(name = " Number of Colonies", labels = comma) +
       geom_smooth(aes(x = numcol, y = yieldpercol), model = lm, size = 1.5)
   })
-  
-  #This is the bins or breaks used in the map
-  bins2  <- seq(min(honey_production_data$totalprod,na.rm=T), max(honey_production_data$totalprod,na.rm=T), l = 8)
-  
-  #This is the reactive function and in this 
-  #The us states map is being produced.
+
+  # This is the bins or breaks used in the map
+  bins2 <- seq(min(honey_production_data$totalprod, na.rm = T), max(honey_production_data$totalprod, na.rm = T), l = 8)
+
+  # This is the reactive function and in this
+  # The us states map is being produced.
   map_nation <- reactive({
     states <- geojsonio::geojson_read("data/us_states.json", what = "sp")
     names(states@data) <- "state"
@@ -134,74 +135,84 @@ my_server <- function(input, output, session) {
       filter(year == input$yearchoice)
     single_year$state <- state.name[match(single_year$state, state.abb)]
     states@data <- left_join(states@data, single_year, by = "state")
-    
-   #This is the function used to fill the color in the map
-   pal <- colorBin("YlOrRd", domain = states@data$totalprod, bins = bins2)
-   
-   #This is the labels
-   labels <- labels <- sprintf(
-     "<strong>%s</strong><br/>%g Honey Production",
-     states$state, states$totalprod
-   ) %>% lapply(htmltools::HTML)
-   
-   #Used to produce the map.
-   leaflet() %>%
+
+    # This is the function used to fill the color in the map
+    pal <- colorBin("YlOrRd", domain = states@data$totalprod, bins = bins2)
+
+    # This is the labels
+    labels <- labels <- sprintf(
+      "<strong>%s</strong><br/>%g Honey Production",
+      states$state, states$totalprod
+    ) %>% lapply(htmltools::HTML)
+
+    # Used to produce the map.
+    leaflet() %>%
       setView(-96, 37.8, 4) %>%
-      addPolygons(data = states,
-                  fillColor = ~pal(states@data$totalprod),
-                  weight = 1,
-                  smoothFactor = 0.5,
-                  dashArray = "3",
-                  color = "blue",
-                  fillOpacity = 0.8,
-                  highlight = highlightOptions(
-                    weight = 5,
-                    color = "#666",
-                    dashArray = "",
-                    fillOpacity = 0.7,
-                    bringToFront = TRUE),
-                  label = labels)%>%
-     addLegend(pal = pal,
-               values = states@data$totalprod,
-               opacity = 0.7,
-               title = "Total Production",
-               position = "bottomleft")
-   })
-  
-  #This renders the map
+      addPolygons(
+        data = states,
+        fillColor = ~ pal(states@data$totalprod),
+        weight = 1,
+        smoothFactor = 0.5,
+        dashArray = "3",
+        color = "blue",
+        fillOpacity = 0.8,
+        highlight = highlightOptions(
+          weight = 5,
+          color = "#666",
+          dashArray = "",
+          fillOpacity = 0.7,
+          bringToFront = TRUE
+        ),
+        label = labels
+      ) %>%
+      addLegend(
+        pal = pal,
+        values = states@data$totalprod,
+        opacity = 0.7,
+        title = "Total Production",
+        position = "bottomleft"
+      )
+  })
+
+  # This renders the map
   output$map <- renderLeaflet({
     map_nation()
   })
-  
-  #This render the value box with avg production.
+
+  # This render the value box with avg production.
   output$avg <- renderValueBox({
-      States <- filter(honey_production_data, year == input$yearchoice) %>%
+    States <- filter(honey_production_data, year == input$yearchoice) %>%
       summarise(TotalAvg = mean(totalprod))
     avg <- round(States$TotalAvg[1], digits = 2)
     valueBox(
-      paste0(avg), "Average Production", icon = icon("product-hunt"),
-      color = "red")
+      paste0(avg), "Average Production",
+      icon = icon("product-hunt"),
+      color = "red"
+    )
   })
-  
-  #This render the value box with year selected
+
+  # This render the value box with year selected
   output$yearselected <- renderValueBox({
     valueBox(
-      paste0(input$yearchoice), "Year Selected", icon = icon("calendar"),
-      color = "blue")
+      paste0(input$yearchoice), "Year Selected",
+      icon = icon("calendar"),
+      color = "blue"
+    )
   })
-  
-  #This render the value box with total prodction
+
+  # This render the value box with total prodction
   output$total <- renderValueBox({
     States <- filter(honey_production_data, year == input$yearchoice) %>%
       summarise(Total = sum(totalprod))
     total <- round(States$Total[1], digits = 2)
     valueBox(
-      paste0(total), "Total Production", icon = icon("product-hunt"),
-      color = "yellow")
+      paste0(total), "Total Production",
+      icon = icon("product-hunt"),
+      color = "yellow"
+    )
   })
 }
-  
-#This is used to take in the server create to 
-#run the app.
-shinyServer(my_server)
 
+# This is used to take in the server create to
+# run the app.
+shinyServer(my_server)
--- ui.R
+++ ui.R
@@ -12,72 +12,87 @@ library(scales)
 my_ui <- dashboardPage(
   dashboardHeader(title = "Honey Production"),
   dashboardSidebar(
-    sidebarMenu( id = "sidebarmenu",
-                 menuItem("Home", tabName = "home", icon = icon("home")),
-                 menuItem("Nation", tabName = "nation", icon = icon("map")),
-                 conditionalPanel("input.sidebarmenu == 'nation'",
-                                  selectInput("yearchoice", label = "Select Year", 
-                                              choices = honey_production_data$year)),
-                 menuItem("State", tabName = "state", icon = icon("map-marker")),
-                 conditionalPanel("input.sidebarmenu == 'state'", 
-                                  sliderInput("obs_year", "Year:", value = 1, min = 1998, max = 2012, sep = ''), 
-                                  selectInput("obs_state", "State:", state.name)),
-                 menuItem("Summary", tabName = "summary", icon = icon("book")),
-                 menuItem("Reference", tabName = "refer", icon = icon("graduation-cap")) 
+    # Includes every menu option to pick from
+    sidebarMenu(
+      id = "sidebarmenu",
+      menuItem("Home", tabName = "home", icon = icon("home")),
+      menuItem("Nation", tabName = "nation", icon = icon("map")),
+      conditionalPanel(
+        "input.sidebarmenu == 'nation'",
+        selectInput("yearchoice",
+          label = "Select Year",
+          choices = honey_production_data$year
+        )
+      ),
+      menuItem("State", tabName = "state", icon = icon("map-marker")),
+      conditionalPanel(
+        "input.sidebarmenu == 'state'",
+        sliderInput("obs_year", "Year:", value = 1, min = 1998, max = 2012, sep = ""),
+        selectInput("obs_state", "State:", state.name)
+      ),
+      menuItem("Summary", tabName = "summary", icon = icon("book")),
+      menuItem("Reference", tabName = "refer", icon = icon("graduation-cap"))
     )
   ),
+
+  # The content in each of the menu options
   dashboardBody(
     tabItems(
-      tabItem(tabName = "nation",
-              box(
-                title = "United States", solidHeader = TRUE,
-                width = 14, status = "primary",
-                leafletOutput('map')
-              ),
-              fluidRow(
-                valueBoxOutput('avg'),
-                valueBoxOutput('yearselected'),
-                valueBoxOutput("total")
-              )),
-      tabItem(tabName = "state",
-              fluidRow(
-                box(
-                  title = "Honey Produced (per State)", width = 6, solidHeader = TRUE, background = "olive",
-                  leafletOutput('country')
-                ),
-                box(
-                  title = "Plot", width = 6, solidHeader = TRUE, background = "olive",
-                  plotOutput('graph_output')
-                ), 
-                box(
-                  title = "Table", width = 12, align = "center" ,solidHeader = TRUE, background = "black", 
-                  tableOutput('table')
-                ) 
-              )
+      tabItem(
+        tabName = "nation",
+        box(
+          title = "United States", solidHeader = TRUE,
+          width = 14, status = "primary",
+          leafletOutput("map")
+        ),
+        fluidRow(
+          valueBoxOutput("avg"),
+          valueBoxOutput("yearselected"),
+          valueBoxOutput("total")
+        )
       ),
-      tabItem(tabName = "summary",
-              box(
-                title = "Amount of Colonies VS Honey Yield per Colony", solidHeader = TRUE,
-                background = "olive",
-                width = 12,
-                plotOutput("scatter", height = 400)
-              )
+      tabItem(
+        tabName = "state",
+        fluidRow(
+          box(
+            title = "Honey Produced (per State)", width = 6, solidHeader = TRUE, background = "olive",
+            leafletOutput("country")
+          ),
+          box(
+            title = "Plot", width = 6, solidHeader = TRUE, background = "olive",
+            plotOutput("graph_output")
+          ),
+          box(
+            title = "Table", width = 12, align = "center", solidHeader = TRUE, background = "black",
+            tableOutput("table")
+          )
+        )
       ),
-      tabItem(tabName = "home",
-              img(src = "bee.JPG"),
-              tags$br(),
-              tags$br(),
-              tags$br(),
-              
-              # Formats sidebar panels, the text of the panels and the intro text
-              tags$head(tags$style((" #sidebar {
+      tabItem(
+        tabName = "summary",
+        box(
+          title = "Amount of Colonies VS Honey Yield per Colony", solidHeader = TRUE,
+          background = "olive",
+          width = 12,
+          plotOutput("scatter", height = 400)
+        )
+      ),
+      tabItem(
+        tabName = "home",
+        img(src = "bee.JPG"),
+        tags$br(),
+        tags$br(),
+        tags$br(),
+
+        # Formats sidebar panels, the text of the panels and the intro text
+        tags$head(tags$style((" #sidebar {
                                     background-color: #FFD54C;
                                     }"))),
-            tags$head(tags$style("#sidebarText{color: black;
+        tags$head(tags$style("#sidebarText{color: black;
                                  font-size: 18px;
                                  font-family: 'Georgia';
                                  }")),
-            tags$head(tags$style((" #intro {color: black;
+        tags$head(tags$style((" #intro {color: black;
                                   font-size: 30px;
                                   text-align: center;
                                   font-family: 'Georgia';
@@ -85,9 +100,7 @@ my_ui <- dashboardPage(
                                   div.box-header {
                                   text-align: center;
                                   }"))),
-              
-            
-            box(id = "intro", width = 12, "Honey production in the U.S. has been on a steady decline 
+        box(id = "intro", width = 12, "Honey production in the U.S. has been on a steady decline 
                 since 1998 and there isn't any sign of it stopping. Various 
                 factors play into why this is the case but the biggest comes 
                 down to the significant decline in honey bee colonies. Due to 
@@ -95,47 +108,55 @@ my_ui <- dashboardPage(
                 production and income. Our hope is that through this analysis 
                 we can better explain the reasons why the industry is struggling
                 and simultaneously raise awareness about a growing problem."),
-            tags$br(),
-            tags$br(),
-            tags$br(),
-            tags$br(),
-            sidebarLayout(
-              sidebarPanel(id = "sidebar",
-                           h1(strong("Colony Collapse Disorder")),
-                           p(id = "sidebarText", 'CCD or Colony Collapse Disorder is defined as "The sudden mass disappearance
+        tags$br(),
+        tags$br(),
+        tags$br(),
+        tags$br(),
+        sidebarLayout(
+          sidebarPanel(
+            id = "sidebar",
+            h1(strong("Colony Collapse Disorder")),
+            p(id = "sidebarText", 'CCD or Colony Collapse Disorder is defined as "The sudden mass disappearance
                              of the majority of worker bees in a colony." In the winter of 2006 bee keepers
                              reported an unsual high loss of bees from their hives yet found very few dead bees
                              near the colony. Due to the substantal loss of worker bees hives that experienced CCD
                              would die. Researchers have blamed The cause of CCD on anything from pesticides
                              to the bees level of stress but the main reason is yet to be discoverd.')
-                           ),
-              sidebarPanel(id = "sidebar",
-                           h1(strong("Overall Questions")),
-                           p(id = "sidebarText", "How did the colony collapse disorder of 2006 affect honey production?",
-                             br(), br(), "How does environment affect honey production?",
-                             br(), br(), "Is there any relation between number of colonies and price per pound in a given year per state?",
-                             br(), br(), "Which state consistently produced more honey than the rest, how is it different than other states?"),
-                           width = 8
-              )
-                           ),
-            sidebarLayout(
-              sidebarPanel(id = "sidebar",
-                           h1(strong("Background")),
-                           p(id = "sidebarText", ""),
-                           width = 8
-              ),
-              sidebarPanel(id = "sidebar",
-                           h1(strong("Resources")),
-                           img(src = "USDA.JPG", height = 55, width = 110),
-                           a("USDA Honey Production", href = "https://www.nass.usda.gov/Surveys/Guide_to_NASS_Surveys/Bee_and_Honey/"),
-                           br(),
-                           br(),
-                           img(src = "EPA.JPG", height = 55, width = 110),
-                           a("Colony Collapse Disorder", href = "https://www.epa.gov/pollinator-protection/colony-collapse-disorder")
-              )
-            )
-            ))),
+          ),
+          sidebarPanel(
+            id = "sidebar",
+            h1(strong("Overall Questions")),
+            p(
+              id = "sidebarText", "How did the colony collapse disorder of 2006 affect honey production?",
+              br(), br(), "How does environment affect honey production?",
+              br(), br(), "Is there any relation between number of colonies and price per pound in a given year per state?",
+              br(), br(), "Which state consistently produced more honey than the rest, how is it different than other states?"
+            ),
+            width = 8
+          )
+        ),
+        sidebarLayout(
+          sidebarPanel(
+            id = "sidebar",
+            h1(strong("Background")),
+            p(id = "sidebarText", ""),
+            width = 8
+          ),
+          sidebarPanel(
+            id = "sidebar",
+            h1(strong("Resources")),
+            img(src = "USDA.JPG", height = 55, width = 110),
+            a("USDA Honey Production", href = "https://www.nass.usda.gov/Surveys/Guide_to_NASS_Surveys/Bee_and_Honey/"),
+            br(),
+            br(),
+            img(src = "EPA.JPG", height = 55, width = 110),
+            a("Colony Collapse Disorder", href = "https://www.epa.gov/pollinator-protection/colony-collapse-disorder")
+          )
+        )
+      )
+    )
+  ),
   skin = "yellow"
-            )
+)
 
 shinyUI(my_ui)
